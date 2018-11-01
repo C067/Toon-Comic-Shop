01ea3ff3ceb20c5bdbc92bff3a65873bb207c43e
Callum <43255759+C067@users.noreply.github.com>
2018-11-01 11:14:03 -0400

    Update sever.js to deal with Login information

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+//Callum Dodge
 
 var express = require("express");
 var app = express();
@@ -8,10 +9,12 @@ var fs = require("fs");
 var bodyParser = require("body-parser");
 var urlencodedParser = bodyParser.urlencoded({extended: false});
 
+
+//JavaScript Files
 var registration = require(path.join(__dirname, '/js/Registration.js'));
+var login = require(path.join(__dirname, '/js/Registration.js'));
 
 var HTTP_PORT = process.env.PORT || 8080;
-
 
 function onHTTPStart() {
     console.log("Listening on " + HTTP_PORT);
@@ -28,18 +31,6 @@ app.get("/", function(req, res) {
 
 app.get("/Registration", function(req, res) {
     res.sendFile(path.join(__dirname, "/Views/Registration.html"));
-});
-
-app.post("/Registration", urlencodedParser, function(req, res) {
-    var Client = req.body;
-
-    fs.appendFile(path.join(__dirname,'/json/register.json'), JSON.stringify(Client, null, 3) , function(err) {
-        if (err) {
-            console.log(err);
-        }
-    });
-
-    res.send({redirect: "/Login"});
 });
 
 app.get("/Login", function(req, res) {
@@ -60,6 +51,41 @@ app.post("/Login", urlencodedParser, function(req, res) {
     });
 
     res.redirect("/Login");
+});
+
+app.post("/", urlencodedParser, (req, res) => {
+    
+    var Client = req.body;
+    
+    try {
+        var login = JSON.parse(fs.readFileSync(path.join(__dirname,'/json/register.json'), 'utf8'));
+    }
+    catch {
+        res.send("Error file not read");
+        res.status(404).redirect("/");
+    }
+
+    var loginData = [];
+    var found = false;
+    for (var cnt = 0; cnt < login.length; cnt++) {
+        if (Client.Email == login[cnt].Email && Client.Password == login[cnt].Password) {
+            console.log("Found user: " + login[cnt].Username);
+            found = true;
+            login[cnt].Login = true;
+            fs.writeFile(path.join(__dirname,'/json/register.json'), JSON.stringify(login, null, 3) , function(err) {
+                if (err) {
+                    console.log(err);
+                }
+            });
+            break;
+        }
+    }
+
+    if (!found) {
+        console.log("No user found");
+    }
+    
+    res.redirect("/");
 });
 
 app.listen(HTTP_PORT, onHTTPStart);
